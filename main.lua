f43f892bdcd77af90d367fe54d422b01bdbb7257
wooter6928196219911 <89206182+wooter6928196219911@users.noreply.github.com>
2022-02-20 21:09:40 +0100

    Commit the thing
    sus

---
+++ main.lua
@@ -0,0 +1,59 @@
+-- Roblox Parser
+--[[
+    TODO:
+    [Can't for now] Remove hard coding
+    [Kinda] Add a auto updater :
+    Example URI: https://s3.amazonaws.com/setup.roblox.com/version-dcabc777160e4d68-content-fonts.zip
+    [X] Make a switch that let's you select app beta or not
+]]
+
+-- Obtain all of the arguments
+local arguments = ""
+for i = 1,#arg do
+    arguments = arguments..arg[i]
+end
+local temp = io.open("C:\\Roblox\\version.txt", "r")
+local temp2 = io.popen("curl --output - http://setup.roblox.com/version")
+if temp:read("*l") == temp2:read("*l") then
+    print("Up to date")
+else
+    print("We're not up to date")
+    os.execute("cd C:\\Roblox & start /wait lua.exe updater.lua")
+end
+-- Variables
+local appBeta = 1
+local token = ""
+local placeLauncher = ""
+local launchTime = ""
+local browserTrackerID = ""
+local combined = ""
+token = arguments:sub(42,369)
+-- We need the URI for the place launcher
+placeLauncher = arguments:sub(413,583)
+-- Parse the URI characters for place launcher
+placeLauncher = placeLauncher:gsub("%%3A",":")
+placeLauncher = placeLauncher:gsub("%%2F","/")
+placeLauncher = placeLauncher:gsub("%%3F","?")
+placeLauncher = placeLauncher:gsub("%%3D","=")
+-- Due to how windows handles ampersands we must turn it into a escape character
+placeLauncher = placeLauncher:gsub("%%26","^&")
+-- Just to be sure
+launchTime = arguments:sub(382,394)
+-- We need the Browser Tracker ID for "seamless play"
+browserTrackerID = arguments:sub(522,532)
+-- Check if app beta was selected then combine it
+if appBeta == 0 then
+    combined = "C:\\Roblox\\rblx\\RobloxPlayerBeta.exe --play -t "..token.." -j "..placeLauncher.." -b "..browserTrackerID.." --launchtime="..launchTime.." --rloc en_us --gloc en_us"
+elseif appBeta == 1 then
+    combined = "C:\\Roblox\\rblx\\RobloxPlayerBeta.exe --app -t "..token.." -j "..placeLauncher.." -b "..browserTrackerID.." --launchtime="..launchTime.." --rloc en_us --gloc en_us" 
+end
+--local combinedPreview = ""
+--combinedPreview = "C:\\Users\\CENSORED\\AppData\\Local\\Roblox\\Versions\\version-dcabc777160e4d68\\RobloxPlayerBeta.exe --app -t ".."censored".." -j ".."censored ".." -b ".."censored".." --launchtime="..launchTime.." --rloc en_us --gloc en_us"
+
+print("Roblox Parser")
+print("Place launcher URI is "..placeLauncher)
+print("Token is "..token)
+print("Launch time is "..launchTime)
+print("Browser Tracker ID is "..browserTrackerID)
+print("Combined is ".. combined)
+ os.execute("start "..combined)
---
+++ updater.lua
@@ -0,0 +1,52 @@
+local ver = io.popen("curl http://setup.roblox.com/version"):read("a")
+print(ver)
+local list = {
+    [1] = {url = "https://s3.amazonaws.com/setup.roblox.com/"..ver.."-content-fonts.zip", name = "rblx\\content\\fonts"},
+    [2] = {url = "https://s3.amazonaws.com/setup.roblox.com/"..ver.."-content-avatar.zip", name = "rblx\\content\\avatar"},
+    [3] = {url = "https://s3.amazonaws.com/setup.roblox.com/"..ver.."-content-configs.zip", name = "rblx\\content\\configs"},
+    [4] = {url = "https://s3.amazonaws.com/setup.roblox.com/"..ver.."-content-models.zip", name = "rblx\\content\\models"},
+    [5] = {url = "https://s3.amazonaws.com/setup.roblox.com/"..ver.."-content-sky.zip", name = "rblx\\content\\sky"},
+    [6] = {url = "https://s3.amazonaws.com/setup.roblox.com/"..ver.."-content-sounds.zip", name = "rblx\\content\\sounds"},
+    [7] = {url = "https://s3.amazonaws.com/setup.roblox.com/"..ver.."-content-textures2.zip", name = "rblx\\content\\textures"},
+    [8] = {url = "https://s3.amazonaws.com/setup.roblox.com/"..ver.."-RobloxApp.zip", name = "rblx"},
+    [9] = {url = "https://s3.amazonaws.com/setup.roblox.com/"..ver.."-ssl.zip", name = "rblx\\ssl"},
+    [10] = {url = "https://s3.amazonaws.com/setup.roblox.com/"..ver.."-shaders.zip", name = "rblx\\shaders"},
+    [11] = {url = "https://s3.amazonaws.com/setup.roblox.com/"..ver.."-content-textures3.zip", name = "rblx\\PlatformContent\\pc\\textures"},
+    [12] = {url = "https://s3.amazonaws.com/setup.roblox.com/"..ver.."-extracontent-luapackages.zip", name = "rblx\\ExtraContent\\LuaPackages"},
+    [13] = {url = "https://s3.amazonaws.com/setup.roblox.com/"..ver.."-extracontent-models.zip", name = "rblx\\ExtraContent\\models"},
+    [14] = {url = "https://s3.amazonaws.com/setup.roblox.com/"..ver.."-extracontent-places.zip", name = "rblx\\ExtraContent\\places"},
+    [15] = {url = "https://s3.amazonaws.com/setup.roblox.com/"..ver.."-extracontent-textures.zip", name = "rblx\\ExtraContent\\textures"},
+    [16] = {url = "https://s3.amazonaws.com/setup.roblox.com/"..ver.."-extracontent-translations.zip", name = "rblx\\ExtraContent\\translations"},
+    [17] = {url = "https://s3.amazonaws.com/setup.roblox.com/"..ver.."-content-terrain.zip", name = "rblx\\PlatformContent\\pc\\terrain"},
+    [18] = {url = "https://s3.amazonaws.com/setup.roblox.com/"..ver.."-content-platform-fonts.zip", name = "rblx\\PlatformContent\\pc\\fonts"}
+}
+-- -d
+print(list[1])
+os.execute("mkdir rblx")
+os.execute("mkdir rblx\\PlatformContent")
+os.execute("mkdir rblx\\PlatformContent\\pc")
+os.execute("mkdir rblx\\content")
+os.execute("mkdir rblx\\ssl")
+os.execute("mkdir rblx\\ExtraContent")
+local g = io.open("rblx\\AppSettings.xml","w+")
+g = g.write(g,[[<?xml version="1.0" encoding="UTF-8"?>
+<Settings>
+<ContentFolder>content</ContentFolder>
+<BaseUrl>http://www.roblox.com</BaseUrl>
+</Settings>
+]])
+g.flush(g)
+g.close(g)
+for i,v in pairs(list) do
+    os.execute("mkdir "..v.name)
+    print("curl "..list[i].url.." > ".."tmp.zip")
+    os.execute("curl "..list[i].url.." > ".."tmp.zip")
+    os.execute("unzip ".."tmp.zip".." -d "..v.name)
+    os.execute("del tmp.zip")
+end
+os.execute("rd /s /q rblx\\content\\fonts\\families")
+os.execute("rd /s /q rblx\\ssl\\_")
+local dell = io.open("version.txt","w+")
+dell:write(ver)
+dell:flush()
+dell:close()
